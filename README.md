ad3396a988ce6dc4f94f71c4d1519c42c1145622
QuantumBird <13707616187@163.com>
2020-04-18 07:31:19 +0800

    变动： 	新增章节一的笔记和部分程序实现 TODO： 	1. 完成章节一剩下的部分 	2. 为prog1添加一幅示例图片”sample.jpg“

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DIP_opencv
 using OpenCV 4 to study digital image processing
+---
+使用OpenCV4 实现 冈萨雷斯 《数字图像处理》中的部分算法
+
---
+++ secion1-basic/prog1/main.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+int main() {
+  Mat img = imread("../sample.jpg");
+  Mat grayimg;
+  cvtColor(img, grayimg, COLOR_BGR2GRAY);
+
+  auto size = grayimg.size();
+  std::cout << "size of image" << size << std::endl;
+  int scale = 2;
+  Size rsize(size.width * scale, size.height * scale);
+
+  Mat dst1, dst2, dst3, dst4;
+
+  resize(grayimg, dst1, rsize, 0, 0, INTER_NEAREST); // 最近邻内插
+  resize(grayimg, dst2, rsize, 0, 0, INTER_LINEAR);  // 双线性内插
+  resize(grayimg, dst3, rsize, 0, 0, INTER_CUBIC);   // 三次样条内插
+  resize(grayimg, dst4, rsize, 0, 0, INTER_LANCZOS4);// Lanczos 内插
+
+  imshow("origional", grayimg);
+  imshow("INTER_NEAREST", dst1);
+  imshow("INTER_LINEAR", dst2);
+  imshow("INTER_CUBIC", dst3);
+  imshow("INTER_LANCZOS4", dst4);
+
+  waitKey(0);
+  return 0;
+}
